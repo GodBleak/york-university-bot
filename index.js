475312b8ff1827178fd3817880ed7a1906c3e73d
GodBleak <38085518+GodBleak@users.noreply.github.com>
2021-01-30 14:29:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ process.title = 'yorkUBot'
 process.chdir(__dirname);
 require('ts-node').register({ transpileOnly: true });
 
+const config = require('./config')
 // Utils
 const { logError } = require('./utils/log');
 
@@ -18,6 +19,20 @@ bot.use(
 	require('./handlers/unmatched'),
 );
 
+bot.command('submitToChannel', ctx => {
+	if(!ctx.message?.reply_to_message) return ctx.reply(`Please reply with this command to the message you'd like to submit`)
+	ctx.telegram.sendCopy(Number(config.chats?.report), ctx.message.reply_to_message, {reply_markup:{InlineKeyboard:[
+		[
+			{text: 'Send To Channel', data: 'sendToChannel'}, {text: 'Reject submission', data: 'deleteMessage'}
+		]
+	]}})
+})
+bot.action('sendToChannel',ctx => {
+	ctx.telegram.sendCopy( -1001374765591, ctx.callbackQuery?.message)
+})
+bot.action('deleteMessage', ctx => {
+	ctx.deleteMessage(ctx.callbackQuery?.message?.message_id)
+})
 bot.catch(logError);
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
