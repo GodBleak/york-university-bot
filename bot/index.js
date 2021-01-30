9ed080a458f5eb7d4757fb984c9366841111f459
GodBleak <38085518+GodBleak@users.noreply.github.com>
2021-01-30 14:52:39 -0700

    Update index.js and index.js

--- bot/index.js
+++ bot/index.js
@@ -11,7 +11,20 @@ const bot = new Telegraf(config.token, {
 	handlerTimeout: ms('5s'),
 	telegram: { apiRoot:'https://bot.api.telegram.secureline.ca' },
 });
-
+bot.command('submitToChannel', ctx => {
+	if(!ctx.message.reply_to_message) return ctx.reply(`Please reply with this command to the message you'd like to submit`)
+	ctx.telegram.sendCopy(Number(config.chats.report), ctx.message.reply_to_message, {reply_markup:{InlineKeyboard:[
+		[
+			{text: 'Send To Channel', data: 'sendToChannel'}, {text: 'Reject submission', data: 'deleteMessage'}
+		]
+	]}})
+})
+bot.action('sendToChannel',ctx => {
+	ctx.telegram.sendCopy( -1001374765591, ctx.callbackQuery.message)
+})
+bot.action('deleteMessage', ctx => {
+	ctx.deleteMessage(ctx.callbackQuery.message.message_id)
+})
 if (process.env.NODE_ENV === 'development') {
 	bot.polling.offset = -1;
 }
